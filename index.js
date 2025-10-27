1c1e795e66743fa4f37c7d8a7d7e7eb1b6abcb1c
Josh Fisher <jfisherd@gmail.com>
2025-10-27 15:08:26 -0400

    both buttons added, list populates, remove button not working

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function filterItems(searchTerm) {
     shoppingListFiltered = []
     for (i=0;i<shoppingList.length;i++) {
         if (shoppingList[i].includes(searchTerm)) {
-            shoppingListFiltered.unshift(shoppingList[i])
+            shoppingListFiltered.push(shoppingList[i])
         }
     }
     return shoppingListFiltered
@@ -46,3 +46,28 @@ function filterItems(searchTerm) {
 // console.log(filterItems("a")) // PLACEHOLDER FUNCTION CALL FOR DEV TESTING
 
 // Task 3
+itemInput = document.getElementById("itemInput");
+additemButton = document.getElementById("addItemButton");
+removeitemButton = document.getElementById("removeItemButton");
+itemList = document.getElementById("itemList");
+
+addItemButton.addEventListener("click", function () {
+  itemText = itemInput.value
+ 
+  if (itemText === "") {
+    alert("Please enter an item!")
+    return
+  }
+ 
+  listItem = document.createElement("li");
+  listItem.innerText = itemText;
+ 
+  itemList.appendChild(listItem)
+  itemInput.value = "" // Clear the input field
+})
+
+itemList.addEventListener("click", function (event) {
+  if (event.target.tagName === "LI") {
+    itemList.removeChild(event.target)
+  }
+})
