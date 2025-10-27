12b2d7a26c3535361739a087eb5b6c1920ad0f5e
Josh Fisher <jfisherd@gmail.com>
2025-10-27 13:45:13 -0400

    task 1 functioning

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 shoppingList = []
 
+// Task 1
 function addItem(item) {
 shoppingList.push(item)
 }
-
 addItem("banananas")
 addItem("aple")
-console.log(shoppingList)
+displayList()
+
+function removeLastItem() {
+    shoppingList.pop()
+}
+removeLastItem()
+displayList()
+
+function displayList() {
+    console.log(shoppingList)
+}
+
