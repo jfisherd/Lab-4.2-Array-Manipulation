8e87a2eae4d1f8d1f6c91991b5bd482cb374003e
Josh Fisher <jfisherd@gmail.com>
2025-10-28 11:01:30 -0400

    remove item fix

--- index.js
+++ index.js
@@ -1,60 +1,38 @@
 shoppingList = []
 
-// Task 1
 function addItem(item) {
-shoppingList.push(item)
+    if (shoppingList.includes(item)) {  // check for duplicate entries
+        return alert(`${item} already on list`)
+    } else {
+        shoppingList.push(item)
+    }
 }
-
-// addItem("Alice") // PLACEHOLDER LIST ITEMS FOR DEV TESTING 
-// addItem("Bob")
-// addItem("Charlie")
-// addItem("aaaa")
-// addItem("a")
-// addItem("David")
-// displayList()
 
 function removeLastItem() {
     shoppingList.pop()
 }
-// removeLastItem() // PLACEHOLDER FUNCTION CALLS FOR DEV TESTING
-// displayList()
 
 function displayList() {
     console.log(shoppingList)
 }
 
-// Task 2
-function addItem(item) {
-    if (shoppingList.includes(item)) {
-        return console.log(`${item} already on list`)
-    } else {
-        shoppingList.push(item)
-    }
-}
-// addItem("Alice") // PLACEHOLDER FUNCTION CALLS FOR DEV TESTING
-// displayList()
-
-// function filterItems(searchTerm) { //PLACEHOLDER, WORKING TASK 2 FUNCTION FOR DEV TESTING, DO NOT REMOVE
-//     shoppingListFiltered = []
-//     for (i=0;i<shoppingList.length;i++) {
-//         if (shoppingList[i].includes(searchTerm)) {
-//             shoppingListFiltered.push(shoppingList[i])
-//         }
-//     }
-//     return shoppingListFiltered
-// }
-
 function filterItems(searchTerm) {
   return shoppingList.filter(x=>x.includes(searchTerm))
 }
 
-// console.log(filterItems("a")) // PLACEHOLDER FUNCTION CALL FOR DEV TESTING
-
-// Task 3
 itemInput = document.getElementById("itemInput");
-additemButton = document.getElementById("addItemButton");
-removeitemButton = document.getElementById("removeItemButton");
+addItemButton = document.getElementById("addItemButton");
+removeItemButton = document.getElementById("removeItemButton");
 itemList = document.getElementById("itemList");
+
+function updateCart() {
+  itemList.innerHTML = ""
+  for (i=0;i<shoppingList.length;i++) {
+    listItem = document.createElement("li")
+    listItem.innerText = shoppingList[i]
+    itemList.appendChild(listItem)
+  }
+}
 
 addItemButton.addEventListener("click", function () {
   itemText = itemInput.value
@@ -63,16 +41,12 @@ addItemButton.addEventListener("click", function () {
     alert("Please enter an item!")
     return
   }
- 
-  listItem = document.createElement("li");
-  listItem.innerText = itemText;
- 
-  itemList.appendChild(listItem)
+  addItem(itemText)
+  updateCart()
   itemInput.value = "" // Clear the input field
 })
 
-itemList.addEventListener("click", function (event) {
-  if (event.target.tagName === "LI") {
-    itemList.removeChild(event.target)
-  }
+removeItemButton.addEventListener("click", function () {
+  removeLastItem()
+  updateCart()
 })
